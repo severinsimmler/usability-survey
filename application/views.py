69fba000ccbba05a0ac3dce095b6ef294199d866
severinsimmler <severin.simmler@posteo.de>
2018-12-04 18:08:12 +0100

    feat: final updates

--- application/views.py
+++ application/views.py
@@ -8,6 +8,12 @@ web = flask.Flask("usability-survey",
                   template_folder=Path("application", "templates"),
                   static_folder=Path("application", "static"))
 
+
+def dump(path, forms):
+    with path.open("w", encoding="utf-8") as textfile:
+        textfile.write(json.dumps(forms, ensure_ascii=False, indent=4))
+
+
 @web.route("/")
 def index():
     return flask.render_template("index.html",
@@ -18,21 +24,25 @@ def privacy():
     return flask.render_template("privacy.html",
                                  view="privacy")
 
-@web.route("/survey")
-def survey():
-    return flask.render_template("survey1_QUESI.html")
+@web.route("/demographic")
+def demographic():
+    return flask.render_template("demographic.html")
+
+@web.route("/quesi", methods=["POST"])
+def quesi():
+    path = Path("fragebogen-demographic.json")
+    dump(path, flask.request.form)
+    return flask.render_template("quesi.html")
+
+@web.route("/nasa", methods=["POST"])
+def nasa():
+    path = Path("fragebogen-quesi.json")
+    dump(path, flask.request.form)
+    return flask.render_template("nasa.html")
 
 @web.route("/thankyou", methods=["POST"])
 def thankyou():
-    data = flask.request.form
-    pseudonym = data["pseudonym"].lower().replace(" ", "-")
-
-    textfile = Path(f"fragebogen-{pseudonym}.json")
-
-    # Dump forms to JSON:
-    with textfile.open("w", encoding="utf-8") as file:
-        file.write(json.dumps(data, ensure_ascii=False, indent=4))
-
-    print(f"INFO: Dumped survey data to '{textfile.absolute()}'.")
+    path = Path("fragebogen-nasa.json")
+    dump(path, flask.request.form)
     return flask.render_template("thankyou.html",
                                  view="thankyou")
--- usability-survey.py
+++ usability-survey.py
@@ -1,5 +1,7 @@
 import application
+import webbrowser
 
 
 if __name__ == "__main__":
+    webbrowser.open("http://127.0.0.1:5000/")
     application.views.web.run(debug=True)
