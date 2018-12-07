33b9be28fdb7e494717e2530411e67efa1332c13
severinsimmler <severin.simmler@posteo.de>
2018-12-07 14:47:52 +0100

    feat: open browser

--- usability-survey.py
+++ usability-survey.py
@@ -3,5 +3,5 @@ import webbrowser
 
 
 if __name__ == "__main__":
-    #webbrowser.open("http://127.0.0.1:5000/")
-    application.views.web.run(debug=True)
+    webbrowser.open("http://127.0.0.1:5000/")
+    application.views.web.run()
