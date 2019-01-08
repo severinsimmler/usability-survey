b43de981dde146f4d581211591124ed154b3d22d
severinsimmler <severin.simmler@posteo.de>
2019-01-08 16:22:58 +0100

    feat: data

---
+++ analysis/usability/sample.py
@@ -0,0 +1,206 @@
+from dataclasses import dataclass
+from pathlib import Path
+import json
+
+import pandas as pd
+import parse
+from statsmodels.stats.power import TTestIndPower
+
+from . import tests
+from . import utils
+
+
+@dataclass
+class Sample:
+    directory: Path
+    pattern: str = "{website}-{collection}"
+
+    @property
+    def nasa(self):
+        scores = list()
+        for proband in self.survey_data:
+            for survey in proband["answers"]:
+                metadata = parse.parse(self.pattern, survey.name)
+                if metadata and metadata["collection"] == "nasa":
+                    score = survey * 5
+                    score = score.sum() / 6
+                    scores.append({"pseudonym": proband["pseudonym"],
+                                   "score": score,
+                                   "website": metadata["website"],
+                                   "survey": metadata["collection"]})
+        data = pd.DataFrame(scores)
+        data.name = "NASA-TLX"
+        return data
+
+    @property
+    def quesi(self):
+        scores = list()
+        for proband in self.survey_data:
+            for survey in proband["answers"]:
+                metadata = parse.parse(self.pattern, survey.name)
+                if metadata and metadata["collection"] == "quesi":
+                    survey = survey + 1
+                    values = list()
+                    k = list()
+                    z = list()
+                    l = list()
+                    v = list()
+                    fe = list()
+                    for item, value in survey.items():
+                        if item in utils.K:
+                            k.append(value)
+                        elif item in utils.Z:
+                            z.append(value)
+                        elif item in utils.L:
+                            l.append(value)
+                        elif item in utils.V:
+                            v.append(value)
+                        elif item in utils.Fe:
+                            fe.append(value)
+                    values.append(pd.Series(k).median())
+                    values.append(pd.Series(z).median())
+                    values.append(pd.Series(l).median())
+                    values.append(pd.Series(v).median())
+                    values.append(pd.Series(fe).median())
+                    score = pd.Series(values).mean()
+                    scores.append({"pseudonym": proband["pseudonym"],
+                                   "score": score,
+                                   "website": metadata["website"],
+                                   "survey": metadata["collection"]})
+        data = pd.DataFrame(scores)
+        data.name = "QUESI"
+        return data
+
+    @property
+    def pre(self):
+        answers = list()
+        for proband in self.survey_data:
+            for survey in proband["answers"]:
+                if survey.name == "pre":
+                    answers.append({"pseudonym": proband["pseudonym"],
+                                    "geschlecht": survey["Geschlecht"],
+                                    "alter": survey["Alter"],
+                                    "bildungsabschluss": survey["bildungsabschluss"],
+                                    "beschäftigung": survey["Derzeitige Beschäftigung bzw. Studienfach"],
+                                    "vorkenntnisse": survey["Websites schon einmal benutzt"]})
+        data = pd.DataFrame(answers)
+        data.name = "Pre-questionnaire"
+        return data
+
+    @property
+    def post(self):
+        answers = list()
+        for proband in self.survey_data:
+            for survey in proband["answers"]:
+                if survey.name == "post":
+                    answers.append({"pseudonym": proband["pseudonym"],
+                                    "besser": survey["Bessere Website"],
+                                    "positiv shirtinator": survey["Positiv bei shirtinator.de"],
+                                    "negativ shirtinator": survey["Negativ bei shirtinator.de"],
+                                    "positiv spreadshirt": survey["Positiv bei spreadshirt.de"],
+                                    "negativ spreadshirt": survey["Negativ bei spreadshirt.de"]})
+        data = pd.DataFrame(answers)
+        data.name = "Post-questionnaire"
+        return data
+
+    @property
+    def feedback(self):
+        values = list()
+        for proband in self.logfile_data:
+            for logfile in proband["logs"]:
+                metadata = parse.parse(self.pattern, logfile.name)
+                if metadata and metadata["collection"] == "feedback":
+                    counts = logfile[1].value_counts()
+                    values.append({"pseudonym": proband["pseudonym"],
+                                   "positiv": counts["pos"] if "pos" in counts.index else 0,
+                                   "negativ": counts["neg"] if "neg" in counts.index else 0,
+                                   "website": metadata["website"]})
+        data = pd.DataFrame(values)
+        data.name = "Feedback"
+        return data
+
+    @property
+    def keyboard(self):
+        values = list()
+        for proband in self.logfile_data:
+            for logfile in proband["logs"]:
+                metadata = parse.parse(self.pattern, logfile.name)
+                if metadata and metadata["collection"] == "keyboard":
+                    counts = logfile[0].count()
+                    values.append({"pseudonym": proband["pseudonym"],
+                                   "tastenanschläge": counts,
+                                   "website": metadata["website"]})
+        data = pd.DataFrame(values)
+        data.name = "Keyboard"
+        return data
+
+    @property
+    def mouse(self):
+        values = list()
+        for proband in self.logfile_data:
+            for logfile in proband["logs"]:
+                metadata = parse.parse(self.pattern, logfile.name)
+                if metadata and metadata["collection"] == "mouse":
+                    counts = logfile[0].count()
+                    values.append({"pseudonym": proband["pseudonym"],
+                                   "clicks": counts,
+                                   "website": metadata["website"]})
+        data = pd.DataFrame(values)
+        data.name = "Mouse"
+        return data
+
+    @property
+    def survey_data(self):
+        for proband in self.directory.glob("*"):
+            path = Path(proband, "fragebogen-daten")
+            answers = utils.process_surveys(path, ".json")
+            yield {"pseudonym": proband.name, "answers": list(answers)}
+
+    @property
+    def logfile_data(self):
+        for proband in self.directory.glob("*"):
+            path = Path(proband, "logfile-daten")
+            logs = utils.process_logfiles(path, ".log")
+            yield {"pseudonym": proband.name, "logs": list(logs)}
+
+    def optimal_size(self, alpha: float = 0.15, power: float = 0.7):
+        sizes = list()
+        for effect in self.effects:
+            name, effect = effect
+            analysis = TTestIndPower()
+            result = analysis.solve_power(effect,
+                                          power=power,
+                                          nobs1=None,
+                                          ratio=1.0,
+                                          alpha=alpha)
+            sizes.append(result)
+        return {"sizes": sizes,
+                "effects": list(self.effects),
+                "median size": pd.Series(sizes).median()}
+    
+    @property
+    def effects(self):
+        collections = [("NASA-TLX", {"x": self.nasa[self.nasa["website"] == "spreadshirt"]["score"],
+                        "y": self.nasa[self.nasa["website"] == "shirtinator"]["score"]}),
+                       ("QUESI", {"x": self.quesi[self.quesi["website"] == "spreadshirt"]["score"],
+                        "y": self.quesi[self.quesi["website"] == "shirtinator"]["score"]}),
+                       ("Positives Feedback", {"x": self.feedback[self.feedback["website"] == "spreadshirt"]["positiv"],
+                        "y": self.feedback[self.feedback["website"] == "shirtinator"]["positiv"]}),
+                       ("Negatives Feedback", {"x": self.feedback[self.feedback["website"] == "spreadshirt"]["negativ"],
+                        "y": self.feedback[self.feedback["website"] == "shirtinator"]["negativ"]}),
+                       ("Tastenanschläge", {"x": self.keyboard[self.keyboard["website"] == "spreadshirt"]["tastenanschläge"],
+                        "y": self.keyboard[self.keyboard["website"] == "shirtinator"]["tastenanschläge"]}),
+                       ("Mausklicks", {"x": self.mouse[self.mouse["website"] == "spreadshirt"]["clicks"],
+                        "y": self.mouse[self.mouse["website"] == "shirtinator"]["clicks"]})]
+        for collection in collections:
+            name, data = collection
+            effect = tests.cohen_d(data["x"], data["y"])
+            yield name, effect
+
+    def plot_power(self, alpha, sample_sizes, figsize=(8, 4)):
+        effect_sizes = list(self.effects)
+        analysis = tests.TTestIndPower()
+        return analysis.plot_power(nobs=sample_sizes,
+                                   alpha=alpha,
+                                   effect_size=effect_sizes,
+                                   figsize=figsize)
---
+++ analysis/usability/tests.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from statsmodels.stats.power import TTestIndPower
+
+
+def cohen_d(x, y):
+    size_x = len(x)
+    size_y = len(y)
+    dof = size_x + size_y - 2
+    return (np.mean(x) - np.mean(y)) / np.sqrt(((size_x - 1) * np.std(x, ddof=1) ** 2 + (size_y - 1) * np.std(y, ddof=1) ** 2) / dof)
+
+
+class TTestIndPower(TTestIndPower):
+    def plot_power(self, dep_var='nobs', nobs=None, effect_size=None,
+                   alpha=0.05, ax=None, title=None, plt_kwds=None, figsize=(10, 8), 
+                   colors=['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33'], **kwds):
+        #if pwr_kwds is None:
+        #    pwr_kwds = {}
+        fig, ax = plt.subplots(figsize=figsize)
+        plt_alpha = 1 #0.75
+        lw = 2
+        if dep_var == 'nobs':
+            for ii, es in enumerate(effect_size):
+                data, es = es
+                power = self.power(es, nobs, alpha, **kwds)
+                ax.plot(nobs, power, lw=1.5, color=colors[ii], label=data + " (" + r"$d = {}$".format('%4.2F' % es) + ")")
+                xlabel = 'Stichprobengröße'
+                ylabel = 'Trennschärfe'
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        return fig
+
+
+def normal_distribution(collections, columns, alpha=0.05):
+    results = dict()
+    for column, data in zip(columns, collections):
+        spreadshirt = data[data["website"] == "spreadshirt"][column]
+        shirtinator = data[data["website"] == "shirtinator"][column]
+        
+        # Spreadshirt data
+        stat, p = scipy.stats.shapiro(spreadshirt)
+        gaussian = True if p > alpha else False
+
+        results[f"{data.name}-spreadshirt-{column}"] = {"stat": round(stat, 3),
+                                                        "p": round(p, 3),
+                                                        "gaussian": gaussian}
+        
+        # Shirtinator data
+        stat, p = scipy.stats.shapiro(shirtinator)
+        gaussian = True if p > alpha else False
+
+        results[f"{data.name}-shirtinator-{column}"] = {"stat": round(stat, 3),
+                                                        "p": round(p, 3),
+                                                        "gaussian": gaussian}
+    return results
+
+
+def variance_homogeneity(collections, columns, alpha=0.05):
+    results = dict()
+    for column, data in zip(columns, collections):
+        spreadshirt = data[data["website"] == "spreadshirt"][column]
+        shirtinator = data[data["website"] == "shirtinator"][column]
+        
+        stat, p = scipy.stats.levene(spreadshirt, shirtinator)
+        homogeneous = True if p > alpha else False
+
+        results[f"{data.name}-{column}"] = {"stat": round(stat, 3),
+                                            "p": round(p, 3),
+                                            "homogeneous": homogeneous}
+    return results
+
+
+def t_test(x, y, alpha):
+    stat, p = scipy.stats.ttest_ind(x, y)
+    equal = True if p > alpha else False
+    return {"stat": round(stat, 3),
+            "p": round(p, 3),
+            "equal": equal}
+
+
+def mann_whitney_u(x, y, alpha):
+    stat, p = scipy.stats.mannwhitneyu(x, y)
+    equal = True if p > alpha else False
+    return {"stat": round(stat, 3),
+            "p": round(p, 3),
+            "equal": equal}
